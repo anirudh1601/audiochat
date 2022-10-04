19259f9baecd3bbc84c0f06c7242063877bbf4a1
anirudh1601 <rnairudhan01@gmail.com>
2022-10-04 07:46:50 +0530

    first commit

---
+++ audiochat/asgi.py
@@ -0,0 +1,30 @@
+"""
+ASGI config for audiochat project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+import multiaudio.routing
+from channels.routing import ProtocolTypeRouter,URLRouter
+from django.core.asgi import get_asgi_application
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.auth import AuthMiddlewareStack
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'audiochat.settings')
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    # Just HTTP for now. (We can add other protocols later.)
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                multiaudio.routing.websocket_urlpatterns
+            )
+        )
+    ),
+})
---
+++ multiaudio/consumers.py
@@ -0,0 +1,64 @@
+from channels.generic.websocket import AsyncWebsocketConsumer,WebsocketConsumer
+from channels.consumer import AsyncConsumer
+import json
+from asgiref.sync import async_to_sync
+import base64
+import io
+import numpy as np
+
+
+
+class ChatConsumer(WebsocketConsumer):
+    def connect(self):
+        self.room_group_name = 'webrtcinternals'
+        
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
+        
+        self.accept()
+        # print(self.channel_name)
+
+        
+
+        print('connected')
+    def disconnect(self,close_code):
+        self.disconnect()
+    def receive(self,text_data=None, bytes_data=None):
+        #print(event)
+        my_string =bytes_data
+        #print(text_data)
+        #print(len(my_string))
+        #print(my_string)
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type':'chat_message',
+                'text':my_string
+            }
+        )
+
+
+    def chat_message(self,event):
+        #print('chat_message',event)
+        message = event['text']
+        #print(message)
+        self.send(bytes_data=(
+            message
+        ))
+        
+
+
+# def to_bytes(s):
+#     if type(s) is bytes:
+#         return s
+#     elif type(s) is str or (sys.version_info[0] < 3 and type(s) is unicode):
+#         return codecs.encode(s, 'utf-8')
+#     else:
+#         raise TypeError("Expected bytes or string, but got %s." % type(s))       
+
+
+        
+        
+        
